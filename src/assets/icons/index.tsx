d1e44f4c18c60dcc716ee5ce075a0223fb410a4c
ezaatama <ezatama33@gmail.com>
2023-09-04 17:11:15 +0700

    index icons

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -6,5 +6,13 @@ import IconPesanan from './iconPesanan.svg'
 import IconPesananActive from './iconPesananActive.svg'
 import AddSaldo from './addSaldo.svg'
 import GetPoint from './getPoint.svg'
+import IconSatuan from './iconSatuan.svg'
+import IconKiloan from './iconKiloan.svg'
+import IconKarpet from './iconKarpet.svg'
+import IconExpress from './iconExpress.svg'
+import IconVip from './iconVIP.svg'
+import IconSetrika from './iconSetrika.svg'
 
-export {IconHome, IconHomeActive, IconAkun, IconAkunActive, IconPesanan, IconPesananActive, AddSaldo, GetPoint}
+export {IconHome, IconHomeActive, IconAkun, IconAkunActive, IconPesanan, IconPesananActive, AddSaldo, GetPoint,
+    IconSatuan, IconKiloan, IconKarpet, IconExpress, IconVip, IconSetrika
+}
