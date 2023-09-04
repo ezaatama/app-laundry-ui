1284ddc2e7194f31588883ed49d4274f55a9fa53
ezaatama <ezatama33@gmail.com>
2023-09-04 17:12:15 +0700

    index utils

--- src/utils/constant.tsx
+++ src/utils/constant.tsx
@@ -3,6 +3,8 @@ import {Dimensions} from 'react-native'
 export const COLOR_PRIMARY = '#55CB95'
 export const COLOR_DISABLE = '#C8C8C8'
 export const COLOR_SECONDARY = '#E0F7EF'
+export const COLOR_GREY = '#F6F6F6'
+export const COLOR_WARNING = '#FF4D00'
 
 //ukuran lebar layar
 
