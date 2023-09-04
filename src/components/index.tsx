8c8a37d497e808bfa053bebc33c3c8a49fb3a42c
ezaatama <ezatama33@gmail.com>
2023-09-04 17:11:33 +0700

    index components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import BottomNavigationBar from './BottomNavBar'
 import Saldo from './Saldo'
 import ButtonIcon from './ButtonIcon'
+import PesananActive from './PesananAktif'
 
-export {BottomNavigationBar, Saldo, ButtonIcon}
+export {BottomNavigationBar, Saldo, ButtonIcon, PesananActive}
